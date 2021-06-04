f554971daed9923b191e055614b2650eeda0bf80
SuperManito <8032484+SuperManito@user.noreply.gitee.com>
2021-06-05 04:33:00 +0800

    bug fix

--- jd.sh
+++ jd.sh
@@ -17,7 +17,7 @@ ListScripts=($(
 ))
 ListOtherScripts=($(
   cd ${ScriptsDir}
-  ls *.js | grep -Eiv "j[drx]_|$(git ls-files)|ShareCodes|AGENTS|index.js|tencentscf.js|Notify|getJDCookie"
+  ls *.js | grep -Eiv "j[drx]_|$(git ls-files)|ShareCodes|AGENTS|index.js|tencentscf.js|Notify|Cookie"
 ))
 ListCron=${ConfigDir}/crontab.list
 
